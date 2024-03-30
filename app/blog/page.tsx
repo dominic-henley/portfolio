779aa6d5102639ccde577efdabd2df1b50fbfd91
dhenley <dominicshenley@gmail.com>
2024-03-30 16:04:14 +1100

    added typewriter effect

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,7 +4,7 @@ import { SanityDocument } from "next-sanity"
 
 export default async function Blog() {
   
-  const BLOG_QUERY = '*[_type == "blog"]';
+  const BLOG_QUERY = "*[_type == 'blog']";
 
   const blogPosts = await client.fetch<SanityDocument[]>(BLOG_QUERY);
   console.log("Fetching blog posts")
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
-export default async function Home() {
-  
+"use client"
+
+import Typewriter from 'typewriter-effect'
+
+export default function Home() {
+
+  const softwareDev = "<span className='text-green-200'>Software Developer</span>"
+
+  const typewriterWords = [softwareDev, "Programmer"]
+
   return (
     <>
-      test
+    <div
+      className="flex justify-center content-center"
+    >
+      <h3>
+        Hi, my name is Dominic. I'm a 
+        <Typewriter 
+          options={{
+            strings: typewriterWords,
+            autoStart: true,
+            loop: true,
+            skipAddStyles: true
+          }}
+        />
+      </h3>
+    </div>
     </>
   )
 }
